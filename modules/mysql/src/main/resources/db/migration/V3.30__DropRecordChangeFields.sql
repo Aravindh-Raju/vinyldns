0d587fbd7e74dd1f312af852c3768cb7d1693c6c
Aravindh-Raju <aravindhraju2000@gmail.com>
2023-08-08 18:37:52 +0530

    remove indexes

--- modules/mysql/src/main/resources/db/migration/V3.30__DropRecordChangeFields.sql
+++ modules/mysql/src/main/resources/db/migration/V3.30__DropRecordChangeFields.sql
@@ -2,5 +2,7 @@ CREATE SCHEMA IF NOT EXISTS ${dbName};
 
 USE ${dbName};
 
+ALTER TABLE record_change DROP INDEX fqdn_index;
+ALTER TABLE record_change DROP INDEX record_type_index;
 ALTER TABLE record_change DROP COLUMN fqdn;
 ALTER TABLE record_change DROP COLUMN record_type;
